f92a14cd677812b62c034d2866a4183743534694
Hamza Daoud <hamzadaoud99@gmail.com>
2023-10-02 22:52:00 +0300

    Done!

--- components/SongsTable.tsx
+++ components/SongsTable.tsx
@@ -1,76 +1,131 @@
+import { useState } from 'react';
 import React, { use } from 'react';
+import { useRouter } from 'next/router';
+import { ImBin } from 'react-icons/im'
+import axios from '../axiosConfig';
 
-const SongsTable = ({ songsData, user, pageNumber }) => {
-  const signOut = ()=>{
+const SongsTable = ({ songsData, user, pageNumber, pagesCount }) => {
+  const router = useRouter();
+  const [search, setSearch] = useState('');
+
+  const handleSearchChange = (e: any) => {
+    setSearch(e.target.value);
+  };
+  const handleSearch = (e: any) => {
+    e.preventDefault();
+    if (search.length) router.push(`/songs?name=${search}`)
+    else router.push('/songs')
+  }
+  const signOut = () => {
     localStorage.removeItem('authToken');
     localStorage.removeItem('user');
     window.location.href = '/'
+  }
+  const deleteSong = (e: any, song:any) => {
+    e.preventDefault();
+    console.log(e)
+    axios.delete('/song',{
+      data:{
+        id: song._id
+      }
+    })
+    .then((response) => {
+      console.log(response)
+      router.push('/songs')
+    })
+    .catch((error) => {
+      console.error(error);
+    })
   }
   return (
     <div className="container">
       <div className='container'>
         <div className='row'>
-          <div className='col-md-6 col-12'>
+          <div className='col-md-2 col-12'>
             <h1>Songs List</h1>
           </div>
-          <div className='col-md-6 col-12' style={{
+          <div className='col-md-6 col-12' style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <form className="form-inline">
+              <div className="input-group">
+                <input onChange={handleSearchChange} type="text" className="form-control" style={{ borderRadius: '5px' }} placeholder="Search By Name..." />
+                <div className="input-group-append">
+                  <button className="btn btn-outline-secondary" onClick={handleSearch}>Search</button>
+                </div>
+              </div>
+            </form>
+          </div>
+          <div className='col-md-4 col-12' style={{
             display: 'flex',
             alignItems: 'center',
             justifyContent: 'end',
-            padding: 0}}>
-              {pageNumber}
-              {user && (user.roleId == 2 || user.roleId == 3) && <a href="/add_song"><button type="submit" className="btn btn-primary" style={{ backgroundColor: '#3874cc', border: 'none', fontFamily: 'Arial, sans-serif', fontSize: '1rem', padding: '0.5rem 1rem', marginRight:'0.5rem' }}>Add Songs</button></a>}
-              <button type="submit" className="btn btn-primary" style={{ backgroundColor: '#dc3545', border: 'none', fontFamily: 'Arial, sans-serif', fontSize: '1rem', padding: '0.5rem 1rem' }} onClick={signOut}>Logout</button>
+            padding: 0
+          }}>
+            <div style={{ marginRight: '15px' }}>
+              <a href={`/songs?page=${pageNumber - 1}`} style={pageNumber == 1 ? { pointerEvents: 'none', color: 'gray', marginRight: '8px' } : { marginRight: '8px' }}>Prev</a>
+              {pageNumber} Out Of {pagesCount}
+              <a href={`/songs?page=${pageNumber + 1}`} style={pageNumber == pagesCount ? { pointerEvents: 'none', color: 'gray', marginLeft: '8px' } : { marginLeft: '8px' }}>Next</a>
+            </div>
+            {user && (user.roleId == 2 || user.roleId == 3) && <a href="/add_song"><button type="submit" className="btn btn-primary" style={{ backgroundColor: '#3874cc', border: 'none', fontFamily: 'Arial, sans-serif', fontSize: '1rem', padding: '0.5rem 1rem', marginRight: '0.5rem' }}>Add Songs</button></a>}
+            <button type="submit" className="btn btn-primary" style={{ backgroundColor: '#dc3545', border: 'none', fontFamily: 'Arial, sans-serif', fontSize: '1rem', padding: '0.5rem 1rem' }} onClick={signOut}>Logout</button>
           </div>
         </div>
       </div>
       {songsData
         ? songsData.map((song, index) => (
-            <div key={index} className="my-3 card">
-              <div className="card-body">
-                <div className="row justify-content-between">
-                  <img
-                    className="col-md-6 col-12 card-img-top"
-                    src={song.coverImage ? song.coverImage : 'https://btlbeats.com/wp-content/themes/uithemebeatstore/assets/images/default-cover-art.png'}
-                    alt={`${song.name} Cover`}
-                    style={{ width: '120px', height: '100px' }}
-                  />
-                  <div className="col-md-2 col-12" style={{
-                      display: 'flex',
-                      
-                      flexDirection: 'column',
-                      justifyContent: 'center'
+          <div key={index} className="my-3 card">
+            <div className="card-body">
+              <div className="row justify-content-between">
+                <img
+                  className="col-md-6 col-12 card-img-top"
+                  src={song.coverImage ? song.coverImage : 'https://btlbeats.com/wp-content/themes/uithemebeatstore/assets/images/default-cover-art.png'}
+                  alt={`${song.name} Cover`}
+                  style={{ width: '120px', height: '100px' }}
+                />
+                <div className="col-md-2 col-12" style={{
+                  display: 'flex',
+
+                  flexDirection: 'column',
+                  justifyContent: 'center'
                 }}>
-                    <h5 className="card-title">{song.name}</h5>
-                    <p className="card-text">By {song.singer}</p>
-                    
-                  </div>
-                  <div className="col-md-4 col-12" style={{
-                      display: 'flex',
-                      alignItems: 'center',
-                      flexDirection: 'column',
-                      justifyContent: 'center'
+                  <h5 className="card-title">{song.name}</h5>
+                  <p className="card-text">By {song.singer}</p>
+
+                </div>
+                <div className="col-md-4 col-12" style={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  flexDirection: 'column',
+                  justifyContent: 'center'
                 }}>
-                    <audio controls>
-                      <source src={song.link} type="audio/mpeg" />
-                      Your browser does not support the audio element.
-                    </audio>
-                  </div>
-                  <div className='col-md-4 col-12' style={{
-                      display: 'flex',
-                      alignItems: 'center',
-                      flexDirection: 'column',
-                      justifyContent: 'center'
+                  <audio controls>
+                    <source src={song.link} type="audio/mpeg" />
+                    Your browser does not support the audio element.
+                  </audio>
+                </div>
+                <div className={user && (user.roleId == 2 || user.roleId == 3) ? 'col-md-3 col-12' : 'col-md-4 col-12'} style={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  flexDirection: 'column',
+                  justifyContent: 'center'
                 }}>
                   <p className="card-text">
-                      Uploaded on {new Date(song.uploadDate).toLocaleDateString()}
-                    </p>
-                    <p className="card-text">Uploaded by {song.createdBy.name}</p>
-                  </div>
+                    Uploaded on {new Date(song.uploadDate).toLocaleDateString()}
+                  </p>
+                  <p className="card-text">Uploaded by {song.createdBy.name}</p>
                 </div>
+                {user && (user.roleId == 2 || user.roleId == 3) && <div className='col-md-1 col-12' style={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  flexDirection: 'column',
+                  justifyContent: 'center'
+                }}>
+                  <button type="submit" className="btn btn-primary" style={{ backgroundColor: '#dc3545', border: 'none', fontFamily: 'Arial, sans-serif', fontSize: '1rem', padding: '0.5rem 1rem' }} onClick={(e)=>{deleteSong(e,song)}}><ImBin /></button>
+                </div>}
+                
               </div>
             </div>
-          ))
+          </div>
+        ))
         : ''}
     </div>
   );
--- pages/songs.tsx
+++ pages/songs.tsx
@@ -8,11 +8,27 @@ const Songs = () => {
   const router = useRouter();
   const [data, setData] = useState(null);
   const [user, setUser] = useState();
-  const [pageNumber, setPage] = useState(1);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [pagesCount, setPagesCount] = useState(null);
 
+  const getSongs = (page:number, name:string) => {
+    axios.get(`/song?page=${page ? page : 1}&name=${name ? name : ''}`)
+    .then((response) =>{
+      console.log(response)
+      setData(response.data.data)
+      setPagesCount(response.data.pagesCount)
+    })
+    .catch((error) => {
+      console.error(error);
+      if(error.response && error.response.data && error.response.data.statusCode == 401){
+        localStorage.removeItem('authToken');
+        router.push('/login')
+      }
+    });
+  }
   useEffect(() => {
     if(router.isReady){
-      const { page } = router.query;
+      const { page, name } = router.query;
       const authToken = localStorage.getItem('authToken');
       const user = localStorage.getItem('user');
       if (!authToken && typeof window !== 'undefined') {
@@ -22,24 +38,15 @@ const Songs = () => {
         if(user !== null){
           setUser(JSON.parse(user))
         }
-        axios.get(`/song?page=${page ? page : 1}`)
-        .then((response) =>{
-          console.log(response)
-          setData(response.data)
-        })
-        .catch((error) => {
-          console.error(error);
-          if(error.response && error.response.data && error.response.data.statusCode == 401){
-            localStorage.removeItem('authToken');
-            router.push('/login')
-          }
-        });
+        setPageNumber(page ? parseInt(page) : 1)
+        
+        getSongs(page, name)
       }
     }
   }, [router.isReady, router.query])
   return (
     <div style={{margin:'60px 0px'}}>
-      <SongsTable songsData={data} user={user} pageNumber={pageNumber}/>
+      <SongsTable songsData={data} user={user} pageNumber={pageNumber} pagesCount={pagesCount} />
     </div>
   );
 };
